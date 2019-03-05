2c0689a7dd6de989c401d2dbe98ee9dede46f672
Lusica1031 <45970406+Lusica1031@users.noreply.github.com>
2019-03-05 09:44:09 +0800

    ALU_final

--- ALU.v
+++ ALU.v
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module ALU(input reg [31:0]X, input reg [31:0]Y, input reg [3:0]AluOP, input reg [4:0]shamt
+module ALU(input wire [31:0]X, input wire [31:0]Y, input wire [3:0]AluOP, input wire [4:0]shamt
 , output reg [31:0]Result, output reg [31:0]Result2, output wire Equal );
 
 assign Equal = (X == Y);
@@ -28,7 +28,7 @@ always@(*)
 begin
 case(AluOP)
     4'b0000: begin Result = Y << shamt; Result2 = 0; end
-    4'b0001: begin Result = Y >>> shamt; Result2 = 0; end
+    4'b0001: begin Result = $signed(Y) >>> shamt; Result2 = 0; end
     4'b0010: begin Result = Y >> shamt; Result2 = 0; end
     4'b0011: begin {Result2, Result} = X * Y; end
     4'b0100: begin Result = (X / Y); Result2 = (X - (Y * Result)); end
@@ -38,8 +38,8 @@ case(AluOP)
     4'b1000: begin Result = (X | Y); Result2 = 0; end
     4'b1001: begin Result = (X ^ Y); Result2 = 0; end
     4'b1010: begin Result = ~(X | Y); Result2 = 0; end
-    4'b1011: begin Result = ($signed(X) <$signed(Y)); Result2 = 0; end
-    4'b1100: begin Result = ($unsigned(X) < $unsigned(Y)); Result2 = 0; end
+    4'b1011: begin Result = !(!($signed(X) < $signed(Y))); Result2 = 0; end
+    4'b1100: begin Result = !(!($unsigned(X) < $unsigned(Y))); Result2 = 0; end
     default: begin Result = 0; Result2 = 0; end
 endcase
 end
